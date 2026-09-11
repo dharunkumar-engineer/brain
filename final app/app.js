64af63f7ee52a69f28db2b914c2750d6288f6801
dharunkumar.engineer <dharunkumar.engineer@gmail.com>
2026-09-11 22:37:35 +0530

    Update app.js

--- final app/app.js
+++ final app/app.js
@@ -1,5 +1,16 @@
+const express = require("express");
+
+const app = express();
+
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Brain Tumour Detection App is running");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
