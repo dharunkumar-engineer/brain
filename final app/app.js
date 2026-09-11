84067574a2722d278f956a36c1b4ca8143229a3e
dharunkumar.engineer <dharunkumar.engineer@gmail.com>
2026-09-11 22:24:14 +0530

    Update app.js

--- final app/app.js
+++ final app/app.js
@@ -1,12 +1,5 @@
-const express = require("express");
-const app = express();
+const PORT = process.env.PORT || 4000;
 
-const PORT = 4000;
-
-app.get("/", (req, res) => {
-    res.send("Server is running successfully!");
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
